3f7df9bf6c951eb7fd26a5fdf60aa5d2c6b655f4
MeisterGitarrenheld <tom-haeschke@hotmail.de>
2019-02-18 20:17:33 +0100

    Tileset

---
+++ BlindChef/Assets/Scripts/PlayerScripts.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScripts : MonoBehaviour
+{
+
+    public float MoveSpeed;
+
+    private Vector2 moveVector;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}

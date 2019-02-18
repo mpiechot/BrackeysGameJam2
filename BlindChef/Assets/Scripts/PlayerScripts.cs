24f467d5c272811d7035993d3f6e47853d8d364b
MeisterGitarrenheld <tom-haeschke@hotmail.de>
2019-02-18 20:26:22 +0100

    character prefab

--- BlindChef/Assets/Scripts/PlayerScripts.cs
+++ BlindChef/Assets/Scripts/PlayerScripts.cs
@@ -8,15 +8,29 @@ public class PlayerScripts : MonoBehaviour
     public float MoveSpeed;
 
     private Vector2 moveVector;
+    private Rigidbody2D rb;
+
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
-        
+        HandleMovement();
     }
+
+    void HandleMovement()
+    {
+        float xMove = Input.GetAxis("Horizontal") * MoveSpeed;
+        float yMove = Input.GetAxis("Vertical") * MoveSpeed;
+
+
+
+        moveVector = new Vector2(xMove, yMove) * Time.deltaTime * 60f;
+        rb.velocity = moveVector;
+    }
+
 }

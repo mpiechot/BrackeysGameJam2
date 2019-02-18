be246ffa55cb668b2e6a4cc326493297f05fcd46
MeisterGitarrenheld <tom-haeschke@hotmail.de>
2019-02-18 20:41:38 +0100

    Outline blocking

---
+++ BlindChef/Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform Target;
+    public float DampingRate;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Target == null)
+            return;
+
+        transform.position = Vector3.Lerp(transform.position, Target.position + Vector3.back, DampingRate);
+
+    }
+}

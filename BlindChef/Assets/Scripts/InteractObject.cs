082b2bd822392c1e800af7279981a1988c1c2887
MeisterGitarrenheld <tom-haeschke@hotmail.de>
2019-02-19 20:25:22 +0100

    AddFood

--- BlindChef/Assets/Scripts/InteractObject.cs
+++ BlindChef/Assets/Scripts/InteractObject.cs
@@ -8,6 +8,14 @@ public abstract class InteractObject : MonoBehaviour
     public bool CanGetIngredient;
     public bool IsEmpty;
 
-    public abstract Essen GetFood();
+    [SerializeField]
+    protected float ProcessTime;
+    protected float currentProcessTime;
+    protected bool isProcessing;
+    protected Essen currentEssen;
 
+    public abstract Essen GetFood();
+    public virtual void AddFood(Essen foodToAdd) { }
+
+    protected virtual void Process() { }
 }
---
+++ BlindChef/Assets/Scripts/InteractObjects/Pot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : InteractObject
+{
+    
+    void Update()
+    {
+        if (isProcessing)
+            Process();
+    }
+
+    protected override void Process()
+    {
+        currentProcessTime += Time.deltaTime;
+    }
+
+    public override void AddFood(Essen foodToAdd)
+    {
+        currentEssen = foodToAdd;
+        IsEmpty = false;
+        isProcessing = true;
+    }
+
+    public override Essen GetFood()
+    {
+        return currentEssen;
+    }
+}
